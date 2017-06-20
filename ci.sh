d66d5071bbcea6201599ee57ef0e49833061b5be
Juan Pablo Santos Rodriguez <juanpablo@apache.org>
2017-06-20 18:26:04 +0200

    Shell script for jbake site generation, using plain Java, intended to be invoked through a Jenkins job, at ASF. Does not download javadocs as part of site generation, and also assumes there's a JBake installation

--- ci.sh
+++ ci.sh
@@ -4,8 +4,8 @@ mvn clean generate-resources
 git checkout asf-site
 git clean -f -d
 git pull origin asf-site
-rm -rf content
-mv target/content content
+find . -type d | grep -v target | grep -v .git | xargs rm -rf
+mv target/content/* ./
 rm -rf target
 git add .
 git commit -m "Automatic Site Publish by Buildbot"
