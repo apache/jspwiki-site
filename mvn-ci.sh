0626457a58a744a284d170a28e7cb7a9e8212aaf
Juan Pablo Santos Rodríguez <juanpablo.santos@gmail.com>
2018-09-14 21:04:22 +0200

    don't try to delete .

--- mvn-ci.sh
+++ mvn-ci.sh
@@ -4,7 +4,7 @@ mvn clean process-resources
 git checkout asf-site
 git clean -f -d
 git pull origin asf-site
-find . -type d | grep -v target | grep -v .git | grep -v apidocs | xargs rm -rf
+find ./* -type d | grep -v target | grep -v .git | grep -v apidocs | xargs rm -rf
 cp -rf target/content/* ./
 rm -rf target
 git add .
